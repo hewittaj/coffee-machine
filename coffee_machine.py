12ce84cca5b457c98214069076cb99411bd317b8
Alex <alexhewitt.j@gmail.com>
2023-08-21 12:08:52 -0500

    add resource checking logic

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,13 +1,26 @@
 import json
 
+AVAILABLE_DRINKS = ['espresso', 'cappuccino', 'latte']
 
-def check_resources():
+
+def check_resources(drink_selected):
     """
     Check the available resources in the coffee machine.
     """
-    available_resources = json.load("data.json")
-    print(available_resources)
-    pass
+    # Variable declarations
+    data = open_json("data.json")
+    available_resources = data['resources']
+    recipes = data['recipes']
+
+    for item in available_resources:
+        if available_resources[item] < recipes[drink_selected][item]:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+        elif available_resources[item] == 0:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+
+    return True
 
 
 def generate_report():
@@ -19,7 +32,7 @@ def generate_report():
     print(f"Water: {available_resources['water']}ml")
     print(f"Milk: {available_resources['milk']}ml")
     print(f"Coffee: {available_resources['coffee']}g")
-    print(f"Money: $%.2f" % round(available_resources['money_in_machine'], 2))
+    print(f"Money: $%.2f" % round(available_resources['money'], 2))
 
 
 def open_json(file_name):
@@ -42,12 +55,17 @@ def run_coffee_machine():
     """
     Main code block to run coffee machine.
     """
-    # Variable declarations
+    # Variable declarations\
+    enough_resources = True
     index = 0
-    user_request = request_user_selection()
+    user_request = request_user_selection().lower()
+
     while user_request != 'off':
         if user_request == 'report':
             generate_report()
+        if user_request in AVAILABLE_DRINKS:
+            enough_resources = check_resources(user_request)
+        print(enough_resources)
         print(f"test + {index}")
         index += 1
 
