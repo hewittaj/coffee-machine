0f3144577c614db458ba9f74726f0785f12c7853
Alex <alexhewitt.j@gmail.com>
2023-08-21 11:21:24 -0500

    update functions and add json data file

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,16 +1,35 @@
-def check_resources(resources):
+import json
+
+
+def check_resources():
+    """
+    Check the available resources in the coffee machine.
+    """
+    available_resources = json.load("data.json")
+    print(available_resources)
     pass
 
-def generate_report(resources):
+
+def generate_report():
     """
     Generate a report of the shows current resource values.
     """
-    for item in resources:
-        print(f"Water: {item['water']}ml")
-        print(f"Milk: {item['milk']}ml")
-        print(f"Coffee: {item['coffee']}g")
-        print(f"Money: ${round(item['money'], 2)}")
-    
+    available_resources = open_json('data.json')['resources']
+
+    print(f"Water: {available_resources['water']}ml")
+    print(f"Milk: {available_resources['milk']}ml")
+    print(f"Coffee: {available_resources['coffee']}g")
+    print(f"Money: $%.2f" % round(available_resources['money_in_machine'], 2))
+
+
+def open_json(file_name):
+    """
+    Helper function that opens up specified json file
+    """
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
+
 
 def request_user_selection():
     """
@@ -18,23 +37,17 @@ def request_user_selection():
     """
     return input("What would you like to drink? (espresso/latte/cappuccino): ")
 
+
 def run_coffee_machine():
     """
     Main code block to run coffee machine.
     """
     # Variable declarations
     index = 0
-    resources = {
-        "water": 500,
-        "milk": 500,
-        "coffee": 500,
-        "money": 500.0
-    }
-
     user_request = request_user_selection()
     while user_request != 'off':
-        if user_request == 'request':
-            pass
+        if user_request == 'report':
+            generate_report()
         print(f"test + {index}")
         index += 1
 
